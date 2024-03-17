aee39093bac995481a862ec47f3b4ecc08c4f5ec
Surender Pratap Singh <rk31151@gmail.com>
2024-03-17 11:44:35 +0530

    home page and listing part done

---
+++ frontend/src/components/pin/Pin.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { Marker, Popup } from 'react-leaflet';
+import './pin.scss';
+import { Link } from 'react-router-dom';
+
+function Pin({ item }) {
+  return (
+    <Marker position={[item.latitude, item.longitude]}>
+      <Popup>
+        <div className="popupContainer">
+          <img src={item.img} alt="icon" />
+          <div className="textContainer">
+            <Link to={`/${item.id}`}>{item.title}</Link>
+            <span>{item.bedroom} bedroom</span>
+            <b>$ {item.price}</b>
+          </div>
+        </div>
+      </Popup>
+    </Marker>
+  );
+}
+
+export default Pin;
